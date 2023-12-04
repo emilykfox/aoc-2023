6b25d288fa7c09c48ff8004ab2eb8b79244977bf
Emily Fox <emilykfox@fastmail.com>
2023-12-03 23:38:59 -0600

    Day 04: use split instead of regex

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -3,22 +3,23 @@ use std::collections::HashSet;
 fn main() {
     let lines = aoc_2023::collect_lines("./inputs/day04.txt").unwrap();
 
-    let number_re = regex::Regex::new(r"(\d+)").unwrap();
     let mut total = 0;
     let mut num_copies = [1; 198];
     let mut total_copies = 0;
     for (card_num, line) in lines.iter().enumerate() {
         let mut winners: HashSet<u32> = HashSet::new();
-        let mut capture_iter = number_re.captures_iter(line);
-        for capture in capture_iter.by_ref().skip(1).take(10) {
-            winners.insert(capture[1].parse().unwrap());
+        let mut numbers = line
+            .split(' ')
+            .filter_map(|string| string.parse::<u32>().ok());
+        for number in numbers.by_ref().take(10) {
+            winners.insert(number);
         }
 
         let mut won = false;
         let mut points = 0;
         let mut current_maybe_copy = card_num;
-        for capture in capture_iter {
-            if winners.contains(&capture[1].parse().unwrap()) {
+        for number in numbers {
+            if winners.contains(&number) {
                 current_maybe_copy += 1;
                 num_copies[current_maybe_copy] += num_copies[card_num];
                 if won {
