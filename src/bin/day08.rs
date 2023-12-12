5a6a1c0ae9b8ecb2a9001d552a90d956f3d725b3
Emily Fox <emilykfox@fastmail.com>
2023-12-12 12:54:31 -0600

    Day 08

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,8 +1,16 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, mem};
 
 struct Node<'a> {
     left: &'a str,
     right: &'a str,
+}
+
+fn gcd(mut m: u64, mut n: u64) -> u64 {
+    while n != 0 {
+        mem::swap(&mut m, &mut n);
+        n %= m;
+    }
+    m
 }
 
 fn main() {
@@ -16,32 +24,12 @@ fn main() {
         exit_map.insert(node, Node { left, right });
     }
 
-    let mut location = "AAA";
-    let mut num_steps = 0;
-    for instruction in instructions.chars().cycle() {
-        match instruction {
-            'L' => {
-                location = exit_map[location].left;
-            }
-            _ => {
-                location = exit_map[location].right;
-            }
-        }
-        num_steps += 1;
-        if location == "ZZZ" {
-            break;
-        }
-    }
-    println!("Part A: {}", num_steps);
-
     let mut journeys = HashMap::new();
-    for location in exit_map
-        .keys()
-        .filter(|location| location.ends_with('A') || location.ends_with('Z'))
-    {
+    for location in exit_map.keys().filter(|location| location.ends_with('A')) {
         journeys.insert(location, location);
     }
-    let mut time_to_z = HashMap::<&str, u32>::new();
+    let mut time_to_z = HashMap::<&str, u64>::new();
+    let mut num_steps = 0;
     for instruction in instructions.chars().cycle() {
         match instruction {
             'L' => {
@@ -56,16 +44,20 @@ fn main() {
             }
         }
         num_steps += 1;
-        for (&&starting_location, &&current_location) in journeys.iter() {
+        for (_, &&current_location) in journeys.iter() {
             if current_location.ends_with('Z') {
-                time_to_z.entry(starting_location).or_insert(num_steps);
+                time_to_z.entry(current_location).or_insert(num_steps);
             }
         }
         if time_to_z.len() == journeys.len() {
             break;
         }
     }
-    println!("time_to_z: {:?}", time_to_z);
+    println!("Part A: {}", time_to_z["ZZZ"]);
 
-    println!("Part B: {}", num_steps);
+    // wtf, the period is equal to warmup time and they all appear in neat cycles?!
+    let co_np_steps = time_to_z
+        .values()
+        .fold(1u64, |acc, &current| acc * (current / gcd(acc, current)));
+    println!("Part B: {}", co_np_steps);
 }
